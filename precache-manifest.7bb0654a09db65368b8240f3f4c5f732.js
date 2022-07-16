715f2fa2a094d4e19cc42c4aee9f7999d7a650b0
RaedElnady <raed.elnady4@gmail.com>
2022-07-16 11:04:02 +0300

    Updates

---
+++ precache-manifest.7bb0654a09db65368b8240f3f4c5f732.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f71c1bd68001fe1740",
+    "url": "/react-app-movies/static/css/main.f85ebb43.chunk.css"
+  },
+  {
+    "revision": "71f71c1bd68001fe1740",
+    "url": "/react-app-movies/static/js/main.71f71c1b.chunk.js"
+  },
+  {
+    "revision": "da68d82e7a9714e19c80",
+    "url": "/react-app-movies/static/css/1.242e6864.chunk.css"
+  },
+  {
+    "revision": "da68d82e7a9714e19c80",
+    "url": "/react-app-movies/static/js/1.da68d82e.chunk.js"
+  },
+  {
+    "revision": "74ed8cee2ae8019a724f",
+    "url": "/react-app-movies/static/js/runtime~main.74ed8cee.js"
+  },
+  {
+    "revision": "ddc833f2204c699dab3ca5048d32d88f",
+    "url": "/react-app-movies/static/media/not-found-3.ddc833f2.png"
+  },
+  {
+    "revision": "b1a1bebb63656b34a23982706f712f71",
+    "url": "/react-app-movies/static/media/fa-regular-400.b1a1bebb.ttf"
+  },
+  {
+    "revision": "cd2b4095e9ce66cde642c3502a4022d9",
+    "url": "/react-app-movies/static/media/fa-brands-400.cd2b4095.woff2"
+  },
+  {
+    "revision": "e8a1ba418ee6d897d1339ef22e6d8e60",
+    "url": "/react-app-movies/static/media/fa-regular-400.e8a1ba41.woff2"
+  },
+  {
+    "revision": "a78ffbbed2d858c61e068e3b756c9988",
+    "url": "/react-app-movies/static/media/fa-brands-400.a78ffbbe.ttf"
+  },
+  {
+    "revision": "738201559a50502aacabdbdb02720910",
+    "url": "/react-app-movies/static/media/fa-solid-900.73820155.ttf"
+  },
+  {
+    "revision": "55b416a8df21f9f987aa352f10d1343b",
+    "url": "/react-app-movies/static/media/fa-solid-900.55b416a8.woff2"
+  },
+  {
+    "revision": "0d6f5f1852affca569715821fe4aa29b",
+    "url": "/react-app-movies/static/media/fa-v4compatibility.0d6f5f18.ttf"
+  },
+  {
+    "revision": "786e6b3373bab47e928c81c26eeccb08",
+    "url": "/react-app-movies/static/media/fa-v4compatibility.786e6b33.woff2"
+  },
+  {
+    "revision": "bc7bd1031ae3fc1ef06015ad904553d0",
+    "url": "/react-app-movies/index.html"
+  }
+];
